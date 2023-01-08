ff0273c5109b024043d6e72b6aa83a1915381538
erfan-karimi <h410mi3@gmail.com>
2023-01-08 19:12:17 +0330

    model product

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+    name = models.CharField(max_length=155)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
