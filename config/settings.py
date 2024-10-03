3f84efad74da3ebbb6ca9f67cb4a53bc0bf89078
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2024-10-03 08:44:28 +0330

    only django

--- config/settings.py
+++ config/settings.py
@@ -136,20 +136,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-#------------------------Email------------------
-EMAIL_BACKENDS = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT =587
-EMAIL_HOST_USER = 'masoud1212u@gmail.com'
-EMAIL_HOST_PASSWORD = 'qfoa hiyr iroc kqjd'
-#------------------------------
-
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-#     ]
-# }
