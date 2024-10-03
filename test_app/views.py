d5c24a50b109c1c82f60fbc38e938e13783a173f
erfan-karimi <100835460+erfan-karimii@users.noreply.github.com>
2024-10-03 08:47:16 +0330

    drf version

--- test_app/views.py
+++ test_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
-def home(request):
-    return render(request,'index.html',{})
+
+class ApiHome(APIView):
+    def get(self,request):
+        return Response("Hello DRF")
